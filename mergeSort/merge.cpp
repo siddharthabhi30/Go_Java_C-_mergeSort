04e8b0f1f4e7d70a5f447fd123f3e6175a4de0ba
Siddharth <32169520+siddharthabhi30@users.noreply.github.com>
2021-01-09 00:11:33 +0530

    Update merge.cpp

--- mergeSort/merge.cpp
+++ mergeSort/merge.cpp
@@ -1,6 +1,23 @@
-// C++ program in-place Merge Sort 
-#include <bits/stdc++.h> 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
+#define ll int64_t
+#include <cstdlib> 
+#include <math.h>
+#include<cstdio>
+#include<cstring>
+#include <math.h>
+#define vi v(n) vector<ll> v(n);
+#define FOR(I, A, B) for (ll I = (A); I <= (B); I++)
+#define fo(i,n) for(ll i=0;i<n;i++)
+#define sz(a) ll((a).size())
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define dbg(x) cout << #x << " = " << x << endl
+#define dbg2(x,y) cout << #x << " = " << x << ", " << #y << " = " << y << endl
+#define dbg3(x,y,z) cout << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl
+#define dbg4(x,y,z,q) cout << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << ", " << #q << " = " << q << endl
+#define scan(char_array) scanf("%[^\n]s",&char_array)
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
 // Merges two subarrays of arr[]. 
 // First subarray is arr[l..m] 
@@ -92,7 +109,7 @@ int main()
 	// arr.push_back(17);
 	// arr.push_back(37);
 	// arr.push_back(20);arr.push_back(72);
-	populate(100000);
+	populate(10000);
 
 	 clock_t start, end; 
   
